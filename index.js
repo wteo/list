3e4a35dbe154670e155ae75a2c5804f95ca2bec2
wteo <wendyteo.wy@gmail.com>
2022-04-03 15:33:55 +1000

    Option 2, Wrapping the lstat method with a promise

--- index.js
+++ index.js
@@ -1,43 +1,29 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const util = require('util'); // to utilize the promisify function that returns a promise function
 
-// don't need to use require statement for "process" as it's one of the few alr built-in within nodeJS
+//Method 2
+//const lstat = util.promisify(fs.lstate);
+
+//Method 3
+const {lstate} = fs.promises;
+
 fs.readdir(process.cwd(), (err, filenames) => {
     if (err) {
         console.log(err);
     }
-    
-    const allStats = Array(filenames.length).fill(null);
-    // Array() constructor is used to create Array objects. 
-    // When a single or more parameter is passed, the value in these parameters become an array.
-    // Array("Apple","Orange") // ["Apple","Orange"]
-    // Array(1,2,3) // [1,2,3]
-    // The exception is when you pass a number in a single parameter. 
-    // The amount of empty elements will be printed instead based on the number given.
-    // If you pass 2 in the paramenter, two elements will be printed in the array.
-    // i.e Array(2) // [empty, empty]
-
-    for (let filename of filenames) {
-        const index = filenames.indexOf(filename); 
-        // to find the index no of each element in filename array.
-
-        fs.lstat(filename, (err, stats) => {
-            if (err) {
-                console.log(err);
-            } 
-            allStats[index] = stats; // so we use the index to fill in the empty elements in allStats array in the same order. 
-
-            const ready = allStats.every((stats) => {
-                return stats;
-            });
-
-            if (ready) {
-                allStats.forEach((stats, index) => {
-                    console.log(filenames[index], stats.isFile());
-                })
-            }
-        });
-    }
-  
+      
 });
+
+// Method 1
+// const lstat = (filename) => {
+//     return new Promise((resolve, reject) => {
+//         fs.lstat(filename, (err, stats) => {
+//         if (err) {
+//             reject(err);
+//         }
+//         resolve(stats);
+//     })
+//     });
+// };
