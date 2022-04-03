2d888dccdd34b7ca34262345c1e51a9eb3c75336
wteo <wendyteo.wy@gmail.com>
2022-04-03 15:46:57 +1000

    Option 2

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ const fs = require('fs');
 const util = require('util'); // to utilize the promisify function that returns a promise function
 
 //Method 2
-//const lstat = util.promisify(fs.lstate);
+//const lstat = util.promisify(fs.lstat);
 
 //Method 3
-const {lstate} = fs.promises;
+//const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), (err, filenames) => {
+fs.readdir(process.cwd(), async (err, filenames) => {
     if (err) {
         console.log(err);
     }
-      
+
+    for (let filename of filenames) {
+        try {
+            const stats = await lstat(filename);
+            console.log(filename, stats.isFile());
+        } catch (err) {
+            console.log(err);
+        }
+    }
 });
 
 // Method 1
@@ -27,3 +35,6 @@ fs.readdir(process.cwd(), (err, filenames) => {
 //     })
 //     });
 // };
+
+// This is still not an ideal approach. Everything is running in a sequential.
+// Hence, it will be slow when you're working with a huge data.
